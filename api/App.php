a8341c01070e9806a361df8d9bbb67423f12c3fb
spont4e <spont4e@gmail.com>
2015-11-07 10:28:31 +0200

    * Ignore config.php

--- api/App.php
+++ api/App.php
@@ -5,4 +5,33 @@
  */
 class App {
 
+
+    public static function doPost($url, $postdata) {
+
+        //url-ify the data for the POST
+        $fields_string = '';
+        foreach ($postdata as $key => $value) {
+            $fields_string .= $key . '=' . $value . '&';
+        }
+        rtrim($fields_string, '&');
+
+        //open connection
+        $ch = curl_init();
+
+        //set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_POST, count($postdata));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+
+        //execute post
+        $result = curl_exec($ch);
+
+        return $result;
+
+        //close connection
+        curl_close($ch);
+    }
+
 }
