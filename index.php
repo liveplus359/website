91d1bd7472a96e8e3682ade68d960579a34e7472
tuseto <tusetoo@gmail.com>
2015-11-07 14:22:31 +0200

    fixedLocation

--- index.php
+++ index.php
@@ -12,20 +12,26 @@
         <script src="js/custom.js"></script>
     </head>
     <body onload="getLocation();">
+
         <p id="location"></p>
+
         <script>
             var x = document.getElementById("location");
+
             function getLocation() {
                 if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition(showPosition);
+                    navigator.geolocation.getCurrentPosition(showPosition, showError);
                 } else {
                     x.innerHTML = "Geolocation is not supported by this browser.";
                 }
             }
+
             function showPosition(position) {
-                x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+                x.innerHTML = "Latitude: " + position.coords.latitude +
+                        "<br>Longitude: " + position.coords.longitude;
             }
-            function denied(error) {
+
+            function showError(error) {
                 switch (error.code) {
                     case error.PERMISSION_DENIED:
                         x.innerHTML = "User denied the request for Geolocation."
