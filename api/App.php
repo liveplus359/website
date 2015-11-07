b5596388cdc3035de632347733966e1f476c8413
spont4e <spont4e@gmail.com>
2015-11-07 14:06:38 +0200

    * Printing result object

--- api/App.php
+++ api/App.php
@@ -16,11 +16,23 @@ class App {
      */
     public static function doGet($url) {
         $obj = json_decode(file_get_contents($url), true);
+        echo "<pre>";
+        print_r($obj);
+        echo "</pre>";
         return $obj;
     }
 
 }
 
 
-$url = $fsURL . "?client_id=" . $foursquareId . "&client_secret=" . $foursquareSecret . "&ll=" . "43.8,25.9" . "&query=" . "sushi" . "&v=20140806" . "&m=foursquare";
-App::doGet($url);
+$url = $fsURL . "?client_id=" . $foursquareId . "&client_secret=" . $foursquareSecret . "&near=" . $city . "&query=" . "healthy" . "&v=20151107" . "&m=foursquare";
+
+echo "<pre>";
+print_r(App::doGet($url));
+echo "</pre>";
+
+if ($_POST['city']) {
+
+    $city = $_POST['city'];
+
+}
