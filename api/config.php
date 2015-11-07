72d0fba1005b06a1235b5a15614c8b9634fc7559
spont4e <spont4e@gmail.com>
2015-11-07 20:43:35 +0200

    + Added getNearVenue()

--- api/config.php
+++ api/config.php
@@ -2,11 +2,12 @@
 /** App Config */
 
 /** Foursquare */
-$fsURL = 'https://api.foursquare.com/v2/venues/search';
+$fsURL = 'https://api.foursquare.com/v2/venues';
 $foursquareId = 'YOFGE4ABFR1SVEWDSGRYTWPJL2NYUNI5M0I2GQVA3EHXMCXV';
 $foursquareSecret = 'IEJUIMQ2MBPUU1W4ZAP50M04I4F0FINWJOQIYBM55T0S0NOA';
 
 /** Facebook */
 $facebookId = '750598631739997';
+$facebookSecret = '53f13e568f3cf0d485538cf579438884';
 
 $cities = array("Sofia", "Plovdiv", "Ruse", "Varna", "Burgas", "Silistra", "Pleven");
